608dac7a99d783c616120aaa71db8424138a6131
mlb21009a <mlb21009a@gmail.com>
2014-01-19 01:33:50 +0900

    Initial Commit

---
+++ PullDownTests/PullDownTests.m
@@ -0,0 +1,34 @@
+//
+//  PullDownTests.m
+//  PullDownTests
+//
+//  Created by 安部力 on 2014/01/19.
+//  Copyright (c) 2014年 マック太郎. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PullDownTests : XCTestCase
+
+@end
+
+@implementation PullDownTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
